27bd2e373ec8ec8cdd244982335f096a7521dffe
laila22haz <hazmirlaila316@gmail.com>
2024-01-11 18:22:44 +0100

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""5. Complex types - list of floats"""
+
+from typing import List, Set, Dict, Tuple
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    - input_list (List): List
+    Return : float result
+    """
+    return sum(input_list)
