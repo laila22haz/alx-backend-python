284ceb10a011c3bba3126a44023546154407a363
laila22haz <hazmirlaila316@gmail.com>
2024-01-11 17:42:39 +0100

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""1. Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str):
+    """
+    - str1 (str): string varible
+    - str2 (str): string variable
+    Return : str result
+    """
+    return str1+str2
