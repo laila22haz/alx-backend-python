f3823f6cf09b70fde89b19b0ecca3f1e8c6d187e
laila22haz <hazmirlaila316@gmail.com>
2024-01-11 18:01:27 +0100

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Basic annotations - add"""
+
+
+from math import floor as fl
+
+
+def floor(n: float) -> int:
+    """
+    - n (float): float varible
+    Return : floor result
+    """
+    return fl(n)
