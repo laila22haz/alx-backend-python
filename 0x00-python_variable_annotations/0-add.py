2c78e1c8757fd003688af506066e17be33bfbe49
laila22haz <hazmirlaila316@gmail.com>
2024-01-11 17:32:22 +0100

    Task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""0. Basic annotations - add"""
 
 def add(a: float, b: float) -> float:
     """
-    -a : float varible
-    -b : float variable
-    -return : float result
+    - a (float): float varible
+    - b (float): float variable
+    
+    Return : float result
     """
     return a + b
