c825ebe70f25f5da1a8c3652ce56828a90a187da
laila22haz <hazmirlaila316@gmail.com>
2024-01-15 18:40:22 +0100

    Do Hard Thing

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int =10) -> float:
     """Write an asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that waits
     for a random delay between 0 and max_delay (included and float value)
