8300c9e1e311fa226d9d2b70ea3f6ea4b4d39813
laila22haz <hazmirlaila316@gmail.com>
2024-01-17 00:35:33 +0100

    Do Hard Things

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+
+import asyncio
+from random import uniform
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List:
+    """collect 10 random numbers using an async comprehensing"""
+    return [number async for number in async_generator()]
