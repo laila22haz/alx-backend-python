e3ab4f91569599ab3b43c7c090e7301a46beadc3
laila22haz <hazmirlaila316@gmail.com>
2024-01-16 11:32:53 +0100

    Do Hard Thing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Create a list of coroutine objects for wait_random"""
     list = []
-    for x in range:
+    for x in range(n):
         list.append(asyncio.create_task(wait_random(max_delay)))
     delay = await asyncio.gather(*list)
     return sorted(delay)
