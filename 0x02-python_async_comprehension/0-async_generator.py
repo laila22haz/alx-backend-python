0c625b7bd144a41ea68dbdff8953d1b5f10dc9ba
laila22haz <hazmirlaila316@gmail.com>
2024-01-17 00:18:09 +0100

    Do Hard Things

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times, each time asynchronously wait 1 second"""
     for _ in range(10):
         await asyncio.sleep(1)
