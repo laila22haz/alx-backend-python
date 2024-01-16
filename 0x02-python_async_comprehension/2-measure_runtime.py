94b5360952e7713908f9fd7c946483447b709753
laila22haz <hazmirlaila316@gmail.com>
2024-01-17 00:57:36 +0100

    Do Hard Things

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,11 +11,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     time_start = time.perf_counter()
-    for _ in range(4):
-        await asyncio.gather(async_comprehension(),
-                             async_comprehension(),
-                             async_comprehension(),
-                             async_comprehension()
-                             )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension()
+                         )
     time_end = time.perf_counter()
     return time_end - time_start
