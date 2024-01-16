1af0dc65a0c05566efbdfa37f5ed55c11da28ae1
laila22haz <hazmirlaila316@gmail.com>
2024-01-16 12:31:31 +0100

    Do Hard Things

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Tasks"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """function  that takes an integer max_delay and returns a asyncio.Task."""
+    return asyncio.get_event_loop().create_task((wait_random(max_delay)))
