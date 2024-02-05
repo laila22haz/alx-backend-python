df37de44438a95fd390f8e2067a0a6a2b2a7bc2f
laila22haz <hazmirlaila316@gmail.com>
2024-02-05 23:52:38 +0100

    WE CAN DO IT git add test_utils.py git add test_utils.py git add test_utils.py git add test_utils.py git add test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,11 @@
 """Test cases for our lovely acssec_nested_map function
 """
 from parameterized import parameterized
+from typing import Dict
 import unittest
-from utils import access_nested_map
+import requests
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,6 +27,21 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             context = access_nested_map(nested_map, path)
             self.assertRaises(context, expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """test the getJson function"""
+    @parameterized.expand([("http://example.com", {"payload": True}),
+                           ("http://holberton.io", {"payload": False})])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_requests) -> Dict:
+        """test the getJeson function"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_requests.return_value = mock_response
+        result = get_json(test_url)
+        mock_requests.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
