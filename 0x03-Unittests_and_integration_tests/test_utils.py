4aeaafa5057290635fefbf595808ece1e1fc22a3
laila22haz <hazmirlaila316@gmail.com>
2024-02-06 01:13:18 +0100

    WE CAN DO IT

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,8 +57,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, '\
-                          a_method', return_value=42
+        with patch.object(TestClass, 'a_method', return_value=42
                           ) as mock_memoize:
 
             mock_response = TestClass()
